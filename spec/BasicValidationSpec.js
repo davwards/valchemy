247c89ef37f963007b9cdb25675049df78a80aff
mattrothenberg <mattrothenberg@gmail.com>
2015-09-10 19:56:47 -0400

    Add pattern validation, chaining support

--- spec/BasicValidationSpec.js
+++ spec/BasicValidationSpec.js
@@ -43,5 +43,171 @@ describe('basic validation', function() {
         expect(this.doesValidate()).toBeTruthy();
       });
     });
+
+    describe('when the value is null', function() {
+      beforeEach(function(){
+        this.value = null;
+        this.allowedLength = 10;
+      });
+
+      it('invalidates', function() {
+        expect(this.doesValidate()).toBeFalsy();
+      });
+    });
+
+    describe('when the value is undefined', function() {
+      beforeEach(function(){
+        this.value = undefined;
+        this.allowedLength = 10;
+      });
+
+      it('invalidates', function() {
+        expect(this.doesValidate()).toBeFalsy();
+      });
+    });
+
+    describe('when the value is numerical 0', function() {
+      beforeEach(function(){
+        this.value = 0;
+        this.allowedLength = 10;
+      });
+
+      it('invalidates', function() {
+        expect(this.doesValidate()).toBeFalsy();
+      });
+    });
+  });
+
+  describe('matchesPattern', function() {
+    beforeEach(function() {
+      this.doesValidate = function() {
+        return new v.BasicValidation()
+          .pattern(this.pattern)
+          .validate(this.value)
+          .valid;
+      }
+    });
+
+    describe('when the value doesn\'t match the pattern', function() {
+      beforeEach(function(){
+        this.value = '12';
+        this.pattern = /^\d{3}$/;
+      });
+
+      it('invalidates', function() {
+        expect(this.doesValidate()).toBeFalsy();
+      });
+    });
+
+    describe('when the value matches the pattern', function() {
+      beforeEach(function(){
+        this.value = '123';
+        this.pattern = /^\d{3}$/;
+      });
+
+      it('validates', function() {
+        expect(this.doesValidate()).toBeTruthy();
+      });
+    });
+
+    describe('when the value is null', function() {
+      beforeEach(function() {
+        this.value = null;
+      });
+
+      describe('and the pattern does not allow empty strings', function() {
+        beforeEach(function(){
+          this.pattern = /^a+$/;
+        });
+
+        it('invalidates', function() {
+          expect(this.doesValidate()).toBeFalsy();
+        });
+      });
+
+      describe('and the pattern does allow empty strings', function() {
+        beforeEach(function(){
+          this.pattern = /^a*$/;
+        });
+
+        it('invalidates', function() {
+          expect(this.doesValidate()).toBeTruthy();
+        });
+      });
+    });
+
+    describe('when the value is undefined', function() {
+      beforeEach(function() {
+        this.value = undefined;
+      });
+
+      describe('and the pattern does not allow empty strings', function() {
+        beforeEach(function(){
+          this.pattern = /^a+$/;
+        });
+
+        it('invalidates', function() {
+          expect(this.doesValidate()).toBeFalsy();
+        });
+      });
+
+      describe('and the pattern does allow empty strings', function() {
+        beforeEach(function(){
+          this.pattern = /^a*$/;
+        });
+
+        it('invalidates', function() {
+          expect(this.doesValidate()).toBeTruthy();
+        });
+      });
+    });
+  });
+
+  describe('a compound validation', function() {
+    beforeEach(function() {
+      this.doesValidate = function() {
+        return new v.BasicValidation()
+          .length(this.allowedLength)
+          .pattern(this.pattern)
+          .validate(this.value)
+          .valid;
+      }
+    });
+
+    describe('when both validations pass', function() {
+      beforeEach(function() {
+        this.allowedLength = 9;
+        this.pattern = /^\d+$/;
+        this.value = '123456789';
+      });
+
+      it('validates', function() {
+        expect(this.doesValidate()).toBeTruthy();
+      });
+    });
+
+    describe('when the second validation fails', function() {
+      beforeEach(function() {
+        this.allowedLength = 9;
+        this.pattern = /^\d+$/;
+        this.value = '1234a6789';
+      });
+
+      it('invalidates', function() {
+        expect(this.doesValidate()).toBeFalsy();
+      });
+    });
+
+    describe('when the first validation fails', function() {
+      beforeEach(function() {
+        this.allowedLength = 9;
+        this.pattern = /^\d+$/;
+        this.value = '12345678';
+      });
+
+      it('invalidates', function() {
+        expect(this.doesValidate()).toBeFalsy();
+      });
+    });
   });
 });
--- valchemy.js
+++ valchemy.js
@@ -1,15 +1,32 @@
-function BasicValidation() { }
+var _ = require('lodash');
+
+function BasicValidation() {
+  this.validators = [];
+}
 
 BasicValidation.prototype.length = function(requiredLength) {
-  this.validator = function(value) {
-    return value.length === requiredLength;
-  };
+  this.validators.push( 
+    function(value) {
+      return value != null && value.length === requiredLength;
+    }
+  );
 
   return this;
 };
 
+BasicValidation.prototype.pattern = function(pattern) {
+  this.validators.push(
+    function(value) {
+      if(value === null || value === undefined) { value = ''; }
+      return !! value.match(pattern);
+    }
+  );
+
+  return this;
+}
+
 BasicValidation.prototype.validate = function(value) {
-  return {valid: this.validator(value)}
+  return { valid: _.every(this.validators, function(validator) { return validator(value) }) };
 };
 
 module.exports = {
