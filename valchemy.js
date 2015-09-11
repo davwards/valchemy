1899389b6a017c34fabbcaf8cc20d3f17f397908
David Edwards <david.edwr@gmail.com>
2015-09-10 20:44:40 -0400

    Extract addValidator logic
    This is a preparatory refactor in order to extract each validator
    into a separate module.

--- valchemy.js
+++ valchemy.js
@@ -4,26 +4,29 @@ function BasicValidation() {
   this.validators = [];
 }
 
-BasicValidation.prototype.length = function(requiredLength) {
-  this.validators.push( 
-    function(value) {
+function addValidator(validatorBuilder) {
+  return function() {
+    this.validators.push(validatorBuilder.apply(this, arguments));
+    return this;
+  }
+}
+
+BasicValidation.prototype.length = addValidator(
+  function(requiredLength) {
+    return function(value) {
       return value != null && value.length === requiredLength;
     }
-  );
+  }
+);
 
-  return this;
-};
-
-BasicValidation.prototype.pattern = function(pattern) {
-  this.validators.push(
-    function(value) {
+BasicValidation.prototype.pattern = addValidator(
+  function(pattern) {
+    return function(value) {
       if(value === null || value === undefined) { value = ''; }
       return !! value.match(pattern);
     }
-  );
-
-  return this;
-}
+  }
+);
 
 BasicValidation.prototype.validate = function(value) {
   return { valid: _.every(this.validators, function(validator) { return validator(value) }) };
