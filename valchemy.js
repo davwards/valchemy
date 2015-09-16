8fa83b5f9be9d3d0d97b676b215a3a63ec744682
David Edwards <david.edwr@gmail.com>
2015-09-15 23:44:01 -0400

    Clean up addValidator, modifyValidator
    Renamed and restructured some things in order to make the functions more
    obvious, and to emphasize their structural similarity.
    
    The fact that the `modifier` needs a `bind(this)` (valchemy.js:18) when
    the `validator` in `addValidator` does not seems weird to me; I suspect
    that they'll converge one way or the other eventually.

--- valchemy.js
+++ valchemy.js
@@ -5,25 +5,23 @@ function BasicValidation() {
   this.messages = [];
 }
 
-function addValidator(validatorBuilder) {
+function addValidator(makeValidator) {
   return function() {
-    this.validators.push(validatorBuilder.apply(this, arguments));
+    var validator = makeValidator.apply(this, arguments);
+    this.validators.push(validator);
     return this;
   }
 }
 
-function modifyValidator(validatorTransform) {
+function modifyValidator(makeModifier) {
   return function() {
-    var normalizedTransform = validatorTransform.apply(this, arguments);
-    if(normalizedTransform.length != 2)
-      throw "Invalid validator modifier! Must have 2 arguments (validator, value).";
+    var modifier = makeModifier.apply(this, arguments).bind(this);
 
     var targetValidator = this.validators.pop();
-    var transformedValidator = function(value) {
-      return normalizedTransform.apply(this, [targetValidator, value]);
-    }.bind(this);
+    this.validators.push(function(value) {
+      return modifier(targetValidator, value);
+    });
 
-    this.validators.push(transformedValidator);
     return this;
   }
 }
