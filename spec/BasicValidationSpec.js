56cbdc16095c1fa48c727bfba1540a1436bbb138
David Edwards <david.edwr@gmail.com>
2016-01-10 12:56:08 -0500

    Remove redundant tests in BasicValidationSpec
    Now that combineResults has been extracted and the forAttribute key is
    gone, the tests in BasicValidationSpec that checked behavior around how
    base errors and attribute errors got combined into the final result seem
    redundant. usageExampleSpec is effectively our suite of "feature" or
    integration tests which confirms that the combineResults method is being
    used properly, so I think the unit coverage around combinedResults is
    sufficient to show that attribute errors are handled correctly.
    
    I'm starting to suspect that we can delete BasicValidationSpec entirely;
    things are broken out enough with independent unit tests that it doesn't
    provide much confidence beyond what usageExampleSpec already gives us.

--- spec/BasicValidationSpec.js
+++ spec/BasicValidationSpec.js
@@ -53,87 +53,17 @@ describe('basic validation', function() {
 
   describe('with a custom error message', function() {
     it('provides the given message on invalidation', function() {
-      var basicValidation = new v.BasicValidation()
+      var basicValidation = new Validation()
           .length(10).withMessage('The name must be 10 characters long');
 
       expect(basicValidation.validate('Matt Rothenberg192').errors).toContain("The name must be 10 characters long");
     })
 
     it('does not provide the given message on validaton', function() {
-      var basicValidation = new v.BasicValidation()
+      var basicValidation = new Validation()
         .length(10).withMessage("The name must be 10 characters long");
       expect(basicValidation.validate('Elizabethe').valid).toBeTruthy();
       expect(basicValidation.validate('Elizabethe').errors).not.toContain("The name must be 10 characters long");
     });
-  });
-
-  describe('when some of the results have a forAttribute key', function() {
-    it('includes only the messages of the results without forAttribute keys in "messages"', function() {
-      var baconValidator = function(value) {
-        return (value === 'bacon') ?
-          valid() : invalid('Why is this not bacon');
-      };
-
-      var threeSquareMealsValidator = function(value) {
-        return (Object.keys(value).length >= 3) ?
-          valid() : invalid('I need three square meals a day!');
-      }
-
-      var menu = { breakfast: 'bacon', lunch: 'salad' };
-
-      var validation = new v.BasicValidation()
-                              .custom(baconValidator).forAttribute('breakfast')
-                              .custom(baconValidator).forAttribute('lunch')
-                              .custom(baconValidator).forAttribute('dinner')
-                              .custom(threeSquareMealsValidator);
-
-      expect(validation.validate(menu).errors).toEqual(['I need three square meals a day!']);
-    });
-
-    it('includes the messages of the results with forAttribute keys in an "attributeMessages" object', function() {
-      var baconValidator = function(value) {
-        return (value === 'bacon') ?
-          valid() : invalid('Why is this not bacon');
-      };
-
-      var threeSquareMealsValidator = function(value) {
-        return (Object.keys(value).length >= 3) ?
-          valid() : invalid('I need three square meals a day!');
-      }
-
-      var menu = { breakfast: 'bacon', lunch: 'salad' };
-
-      var validation = new v.BasicValidation()
-                              .custom(baconValidator).forAttribute('breakfast')
-                              .custom(baconValidator).forAttribute('lunch')
-                              .custom(baconValidator).forAttribute('dinner')
-                              .custom(threeSquareMealsValidator);
-
-      expect(validation.validate(menu).attributeErrors).toEqual({
-        lunch: invalid('Why is this not bacon'),
-        dinner: invalid('Why is this not bacon')
-      });
-    });
-
-    it('returns an empty attributeMessages object when none of the results have a forAttribute key', function() {
-      var baconValidator = function(value) {
-        return (value === 'bacon') ?
-          valid() : invalid('Why is this not bacon');
-      };
-
-      var threeSquareMealsValidator = function(value) {
-        return (Object.keys(value).length >= 3) ?
-          valid() : invalid('I need three square meals a day!');
-      }
-
-      var menu = { breakfast: 'bacon', lunch: 'salad' };
-
-      var validation = new v.BasicValidation()
-                              .custom(baconValidator)
-                              .custom(threeSquareMealsValidator);
-
-      expect(validation.validate(menu).attributeErrors).toEqual({});
-    });
-
   });
 });
