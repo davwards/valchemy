20d5e70807bb48d92b704e68c793a95e0d2be5ae
Matt Rothenberg <mattrothenberg@gmail.com>
2016-01-13 23:28:00 -0500

    Add maxlength validator. Still unsure of how to handle numerical 0 case, as well as null + undefined error messaging

---
+++ spec/validators/maxlengthSpec.js
@@ -0,0 +1,70 @@
+var maxlengthValidator = require('../../validators/maxlength');
+
+describe('maxlength validator', function() {
+	beforeEach(function() {
+		this.validationResult = function() {
+			var validator = maxlengthValidator(this.maximumLength);
+			var valueToValidate = this.value;
+			var result = validator(valueToValidate);
+			return result;
+		}
+
+		this.doesValidate = function() {
+			return this.validationResult().isValid();
+		}
+
+		this.message = function() {
+			return this.validationResult().errors[0];
+		}
+	})
+
+	describe('when the value is longer than allowed', function() {
+		beforeEach(function() {
+			this.value = '12345'
+			this.maximumLength = 4;
+		});
+
+		it('invalidates', function() {
+			expect(this.doesValidate()).toBeFalsy();
+			expect(this.message()).toEqual('Must be less than or equal to 4 characters long.');
+		});
+	});
+
+	describe('when the value is shorter than allowed', function() {
+		beforeEach(function() {
+			this.value = '123'
+			this.maximumLength = 4;
+		});
+
+		it('validates', function() {
+			expect(this.doesValidate()).toBeTruthy();
+      		expect(this.message()).toBeUndefined();
+		});
+	});
+
+	describe('when the value is null', function() {
+	  beforeEach(function(){
+	    this.value = null;
+	    this.maximumLength = 10;
+	  });
+
+	  it('invalidates', function() {
+	    expect(this.doesValidate()).toBeFalsy();
+	    expect(this.message()).toEqual('Must be at least one character long.');
+	  });
+	});
+
+	describe('when the value is undefined', function() {
+	  beforeEach(function(){
+	    this.value = undefined;
+	    this.maximumLength = 10;
+	  });
+
+	  it('invalidates', function() {
+	    expect(this.doesValidate()).toBeFalsy();
+	    expect(this.message()).toEqual('Must be at least one character long.');
+	  });
+	});
+
+	// How do we want to handle numerical 0 in this case?
+});
